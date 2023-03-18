6bc07bbf88e6c5f1843653b1d92b1e40d1ea4392
rielgbrl <rielgasy9@gmail.com>
2023-03-19 00:37:56 +0700

    add period selection

--- components/MainNavigation/index.js
+++ components/MainNavigation/index.js
@@ -5,20 +5,46 @@ import Link from 'next/link';
 import Gradient from 'rgt'
 import SecondaryButton from '../SecondaryButton';
 import ThirdButton from '../ThirdButton';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import React from 'react';
+import { Box } from '@mui/material';
 
 const MainNavigation = (props) => {
+    const [age, setAge] = React.useState("");
+
+    const handleChange = (event) => {
+      setAge(event.target.value);
+    };
     return (
       <div className={classes.container}>
         <div className={classes.main_page_container}>
+          <div className={classes.role}>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl size='small' fullWidth>
+                <InputLabel id="demo-simple-select-label">Periode</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="Periode"
+                  onChange={handleChange}
+                >
+                  <MenuItem value={2022}>2022</MenuItem>
+                  <MenuItem value={2023}>2023</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
           <div className={classes.role}>
             <ThirdButton>
               <p className={classes.role_detail}>Superuser</p>
             </ThirdButton>
           </div>
           <p className={classes.role}>
-            <span className={classes.email}>
-              superuser@pemilra.uajy.ac.id
-            </span>
+            <span className={classes.email}>superuser@pemilra.uajy.ac.id</span>
           </p>
           <Avatar sx={{ bgcolor: "#DE631C" }}>N</Avatar>
         </div>
