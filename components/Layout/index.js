577ccfeb164386bace718f6fed8acd3e2b219da5
rielgbrl <rielgasy9@gmail.com>
2023-03-23 12:02:37 +0700

    add users

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,65 +1,56 @@
-
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import MainNavigation from "../MainNavigation";
 import SideBar from "../SideBar";
-import classes from './index.module.css'
-
+import classes from "./index.module.css";
 
 const Layout = (props) => {
   const [isMobile, setIsMobile] = useState(false);
-  const [showSidebar, setShowSidebar] = useState(true); 
+  const [showSidebar, setShowSidebar] = useState(true);
 
-  
-
-   const handleResize = () => {
-     if (window.innerWidth < 768) {
-       setIsMobile(true);
-     } else {
-       setIsMobile(false);
-     }
-   };
-
-   useEffect(() => {
-     window.addEventListener("resize", handleResize);
-
-     if (window.innerWidth < 768) {
-       setIsMobile(true);
-      } else {
-        setIsMobile(false);
-     }
-   });
-
-   useEffect(() => {
-    if(isMobile){
-      setShowSidebar(false)
+  const handleResize = () => {
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
     }
-   }, [isMobile])
+  };
 
-  
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
 
-   const handleClick = () => {
-    if(showSidebar){
+    if (window.innerWidth < 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  });
+
+  useEffect(() => {
+    if (isMobile) {
       setShowSidebar(false);
-    }else{
+    }
+  }, [isMobile]);
+
+  const handleClick = () => {
+    if (showSidebar) {
+      setShowSidebar(false);
+    } else {
       setShowSidebar(true);
     }
-    
-   }
+  };
 
-
-  
   return (
     <>
       <MainNavigation sidebarStatus={showSidebar} handle={handleClick} />
       <main className={classes.main}>
         <div className={classes.main_content}>
-          {showSidebar ? <SideBar show={showSidebar} ></SideBar> : null}
+          {showSidebar ? <SideBar show={showSidebar}></SideBar> : null}
 
           <div className={classes.main_main_content}>{props.children}</div>
         </div>
       </main>
     </>
   );
-}
+};
 
 export default Layout;
--- src/pages/users/index.js
+++ src/pages/users/index.js
@@ -1,11 +1,81 @@
+import { Box } from '@mui/material'
+import {Grid} from '@mui/material'
 import classes from './index.module.css'
 import Layout from '../../../components/Layout'
+import {CardContent} from '@mui/material'
+import CustomTableContainer from '../../../components/CustomTableContainer'
+import RoleItem from '../../../components/RoleItem'
+import ThirdButton from '../../../components/ThirdButton'
+import SecondaryButton from '../../../components/SecondaryButton'
+import DataTable from "react-data-table-component";
+
+import React from 'react'
 const Users = () => {
+    const columns = [
+      {
+        name: "No",
+        selector: (row) => row.no,
+        sortable: true,
+      },
+      {
+        name: "Nama",
+        selector: (row) => row.nama,
+        sortable: true,
+      },
+      {
+        name: "Angkatan",
+        selector: (row) => row.angkatan,
+        sortable: true,
+      },
+      {
+        name: "Prodi",
+        selector: (row) => row.prodi,
+        sortable: true,
+      },
+      {
+        name: "NPM",
+        selector: (row) => row.npm,
+        sortable: true,
+      },
+      {
+        name: "Email",
+        selector: (row) => row.email,
+        sortable: true,
+      },
+    ];
+
+    const data = [
+      {
+        id: 1,
+        no: 1 ,
+        nama: "Gabriel Allba",
+        angkatan: "2021",
+        prodi: "Informatika",
+        npm: "210711150",
+        email: "210711150@students.uajy.ac.id",
+      },
+      {
+        id: 2,
+        no: 2 ,
+        nama: "Gabriel Allba",
+        angkatan: "2021",
+        prodi: "Informatika",
+        npm: "210711150",
+        email: "210711150@students.uajy.ac.id",
+      },
+
+    ];
     return (
-        <Layout>
-            <p>users page</p>
-        </Layout>
-    )
+      <Layout>
+        <Box style={{ padding: "1rem" }}>
+          
+                
+                  <DataTable columns={columns} data={data} />
+                
+              
+        </Box>
+      </Layout>
+    );
 }
 
 export default Users
